b2d79fa970a81ade6fe2e18d6dfd2853269a909a
Treaders49 <68151857+Treaders49@users.noreply.github.com>
2022-05-16 22:26:12 +0100

    Update keyboard.java

--- keyboard.java
+++ keyboard.java
@@ -1,11 +1,11 @@
 
-public class keyboard extends Item {
+public class keyboard extends Item { //is a subclass of Item object
 	private String layout;
 	public keyboard(String barcode, String deviceType, String style, String brand, String colour, String connectivity, int quantity, float originalCost, float retailPrice, String layout) {
 		super(barcode, deviceType, style, brand, colour, connectivity, quantity, originalCost, retailPrice);
 		this.setLayout(layout);
 	}
-	public String getLayout() {
+	public String getLayout() { //has a unique attribute layout
 		return layout;
 	}
 	public void setLayout(String layout) {
