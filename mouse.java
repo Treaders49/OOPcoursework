df7d22ad9f5f4df5968dee9eed1837fe6450bfc0
Treaders49 <68151857+Treaders49@users.noreply.github.com>
2022-05-16 22:27:50 +0100

    Update mouse.java

--- mouse.java
+++ mouse.java
@@ -1,11 +1,11 @@
 
-public class mouse extends Item{
+public class mouse extends Item{ //a subclass of Item class
 	private int numberOfButtons;
 	public mouse(String barcode, String deviceType, String style, String brand, String colour, String connectivity, int quantity, float originalCost, float retailPrice,int numberOfButtons) {
 		super(barcode, deviceType, style, brand, colour, connectivity, quantity, originalCost, retailPrice);
 		this.setNumberOfButtons(numberOfButtons);
 	}
-	public int getNumberOfButtons() {
+	public int getNumberOfButtons() { //has a unique attribute numberOfButtons
 		return numberOfButtons;
 	}
 	public void setNumberOfButtons(int numberOfButtons) {
