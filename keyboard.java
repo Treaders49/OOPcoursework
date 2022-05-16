bc875dea0675fecab2dc22beaabe97cf4d9831ec
Treaders49 <68151857+Treaders49@users.noreply.github.com>
2022-05-16 22:28:39 +0100

    Update keyboard.java

--- keyboard.java
+++ keyboard.java
@@ -1,5 +1,5 @@
 
-public class keyboard extends Item { //is a subclass of Item object
+public class keyboard extends Item { //is a subclass of Item class
 	private String layout;
 	public keyboard(String barcode, String deviceType, String style, String brand, String colour, String connectivity, int quantity, float originalCost, float retailPrice, String layout) {
 		super(barcode, deviceType, style, brand, colour, connectivity, quantity, originalCost, retailPrice);
